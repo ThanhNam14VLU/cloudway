aec86e2e84181645f5c609af94364996c85e7515
ThanhNam14VLU <2200420@st.vlute.edu.vn.com>
2025-09-17 23:23:02 +0700

    add feature component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
+import { Feature } from './feature/feature';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Footer],
+  imports: [RouterOutlet,Footer,Feature],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
